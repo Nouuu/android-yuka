b2f51f81669628001ab4f5e9f1eac2ae856fd23e
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2021-11-25 09:23:04 +0100

    menu tab v1

--- app/src/main/java/fr/larrieu_lacoste/noe/ce_que_vous_voulez/ProductDetailsFragment.kt
+++ app/src/main/java/fr/larrieu_lacoste/noe/ce_que_vous_voulez/ProductDetailsFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.product_details_fragment.*
+import java.lang.Exception
 
 class ProductDetailsFragment : Fragment() {
     override fun onCreateView(
@@ -21,6 +23,21 @@ class ProductDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        product_details_bottom_nav.setOnNavigationItemSelectedListener {
+            childFragmentManager.beginTransaction()
+                .replace(
+                    R.id.product_details_nav_host,
+                    when (it.itemId) {
+                        R.id.tab_fiche -> ProductDetailsSummaryFragment()
+                        R.id.tab_nutrition -> ProductDetailsNutritionFragment()
+                        R.id.tab_tableau -> ProductDetailsNutritionnalValuesFragment()
+                        else -> throw Exception("Non géré")
+                    }
+                ).commitAllowingStateLoss()
+
+            true
+        }
     }
 
 }
