23d7ee3aab94648eb0e8cd47602188f7e8c6fd5c
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2021-12-01 18:06:21 +0100

    table not done yet but started

--- app/src/main/java/fr/larrieu_lacoste/noe/ce_que_vous_voulez/features/product_details/nutrional_values/ProductDetailsNutritionnalValuesFragment.kt
+++ app/src/main/java/fr/larrieu_lacoste/noe/ce_que_vous_voulez/features/product_details/nutrional_values/ProductDetailsNutritionnalValuesFragment.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableRow
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import fr.larrieu_lacoste.noe.ce_que_vous_voulez.R
+import fr.larrieu_lacoste.noe.ce_que_vous_voulez.features.product_details.ProductDetailsFragment
+import fr.larrieu_lacoste.noe.ce_que_vous_voulez.features.product_details.ProductDetailsFragmentArgs
+import fr.larrieu_lacoste.noe.ce_que_vous_voulez.model.Product
+import kotlinx.android.synthetic.main.product_details_nutritional_values_fragment.*
 
 class ProductDetailsNutritionnalValuesFragment : Fragment() {
     override fun onCreateView(
@@ -22,6 +28,11 @@ class ProductDetailsNutritionnalValuesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val detailsFragment: ProductDetailsFragment =
+            requireParentFragment().requireParentFragment() as ProductDetailsFragment
+        val product: Product =
+            ProductDetailsFragmentArgs.fromBundle(detailsFragment.requireArguments()).product
+        println(product)
     }
 
 }
