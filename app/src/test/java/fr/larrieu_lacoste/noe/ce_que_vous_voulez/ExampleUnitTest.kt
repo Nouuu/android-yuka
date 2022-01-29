1fc4874e272a8d20c86eee5d3b6e5cd83f4a2b95
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-01-29 12:35:11 +0100

    optimize imports

--- app/src/test/java/fr/larrieu_lacoste/noe/ce_que_vous_voulez/ExampleUnitTest.kt
+++ app/src/test/java/fr/larrieu_lacoste/noe/ce_que_vous_voulez/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package fr.larrieu_lacoste.noe.ce_que_vous_voulez
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
